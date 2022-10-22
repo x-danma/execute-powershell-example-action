f78bca1e8629fd5f65095b644f73903692f1d65b
Daniel Mårtensson <daniel.martensson89@outlook.com>
2022-10-22 23:52:11 +0200

    exec powershell script

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,33 @@
 import core from '@actions/core';
+import exec, { ExecOptions } from '@actions/exec';
 
 try {
-  // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet') || 'World';
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
+
+  let myOutput = '';
+  let myError = '';
+
+  const options: ExecOptions = {};
+  options.listeners = {
+    stdout: (data: Buffer) => {
+      myOutput += data.toString();
+    },
+    stderr: (data: Buffer) => {
+      myError += data.toString();
+    }
+  };
+  options.cwd = './lib';
+
+  await exec.exec('./example.ps1', null, options);
+
+  console.log(`myOutput ${myOutput}!`);
+  console.log(`myError ${myError}!`);
   console.log(`time ${time}!`);
   core.setOutput("returnEcho", time);
+
+
 } catch (error) {
   core.setFailed(error.message);
 }
