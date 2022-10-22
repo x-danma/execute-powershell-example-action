ff350c515314f6c5b48dccad0241ed290cc6b4d7
Daniel Mårtensson <daniel.martensson89@outlook.com>
2022-10-23 00:25:05 +0200

    remove ts (#1)
    * remove ts
    
    * fix

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-import core from '@actions/core';
-import exec from '@actions/exec';
-
+const core = require('@actions/core');
 try {
+  // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet') || 'World';
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
-
-  const output = await exec.getExecOutput('./example.ps1');
-
-  console.log(`output ${output}!`);
   console.log(`time ${time}!`);
   core.setOutput("returnEcho", time);
-
-
+  // Get the JSON webhook payload for the event that triggered the workflow
 } catch (error) {
-  console.log(`Catch error ${error}!`);
   core.setFailed(error.message);
 }
